4ad8c8cb62ae10900377d7c451cd8d4fe8607c87
Maurizio Basaglia <62178459+maurizuki@users.noreply.github.com>
2025-02-25 18:33:32 +0100

    Add files via upload

---
+++ docs/visited-countries.js
@@ -0,0 +1,25 @@
+const sonicSilver = "#747474";
+const oriolesOrange = "#f34f1c";
+const appleGreen = "#7fbc00";
+const selectiveYellow = "#ffba01";
+const vividCerulean = "#01a6f0";
+
+const visitedCountries = {
+    AE: appleGreen,
+    AT: oriolesOrange,
+    CH: appleGreen,
+    DE: vividCerulean,
+    DK: oriolesOrange,
+    ES: oriolesOrange,
+    FR: selectiveYellow,
+    GB: vividCerulean,
+    HR: appleGreen,
+    HU: vividCerulean,
+    IE: appleGreen,
+    IM: oriolesOrange,
+    IT: sonicSilver,
+    JP: selectiveYellow,
+    SI: selectiveYellow,
+    US: vividCerulean,
+    TZ: oriolesOrange,
+};
